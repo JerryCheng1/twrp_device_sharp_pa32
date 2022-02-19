259519591e74882bfdcb38f1a1c05c1998fbe844
Jerry Zheng <jerrycheng.gd@gmail.com>
2022-02-19 03:41:49 +0000

    Kernel: change to source complied

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),PA32)
+ifeq ($(TARGET_DEVICE),pa32)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 #used to add device to Omni's lunch
-add_lunch_combo omni_PA32-userdebug
+add_lunch_combo omni_pa32-userdebug
